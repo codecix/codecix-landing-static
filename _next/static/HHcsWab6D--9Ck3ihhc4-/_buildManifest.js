9ba8ec1f0184fca34ba435a02ef092361040cfef
alvaro-barrera <barrerav.alvaro@gmail.com>
2023-03-05 15:32:39 -0500

    gh-pages

---
+++ _next/static/HHcsWab6D--9Ck3ihhc4-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/713-8e1791e3d7eea676.js","static/chunks/pages/index-bca23f07eb215496.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
